9e9f01e5c05ffa71b8d8b969df9b5916413eea9b
Mohamed Hashemi <dothashemi@gmail.com>
2021-02-03 13:40:11 +0330

    initial commit

---
+++ core/website/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from website.views import index
+
+
+urlpatterns = [
+    path('', index, name='website.index'),
+]
---
+++ core/website/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views import View
+
+
+def index(request):
+    return render(request, 'index.html')
